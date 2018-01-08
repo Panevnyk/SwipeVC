56139dd4dd1dc53ed6e679e58032cea1dae912c3
panevnyk <sieter3008@gmail.com>
2018-01-08 17:42:46 +0200

    Initial Commit

---
+++ SwipeVC/SwipeVC.h
@@ -0,0 +1,19 @@
+//
+//  SwipeVC.h
+//  SwipeVC
+//
+//  Created by Panevnyk Vlad on 1/8/18.
+//  Copyright © 2018 Panevnyk Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeVC.
+FOUNDATION_EXPORT double SwipeVCVersionNumber;
+
+//! Project version string for SwipeVC.
+FOUNDATION_EXPORT const unsigned char SwipeVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeVC/PublicHeader.h>
+
+
